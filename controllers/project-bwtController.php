4d336a3c5cdf4632fbbc85ff4af8d5e135908bb8
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-08-29 21:22:40 +0200

    Added project-bwt page

--- controllers/project-bwtController.php
+++ controllers/project-bwtController.php
@@ -1,5 +1,3 @@
 <?php 
     
-    include("views/page/project-bwt.php");
-    
-?>
+    include("views/page/projects/project-bwt.php");
---
+++ views/page/projects/project-bwt.php
@@ -0,0 +1,28 @@
+<?php
+
+$pageTitle = "Belgium Women Together";
+$pagePath = "project-bwt";
+$projectLead = "Belgium Women Together is a microsite created especially to advertise a religious retreat aimed at women from English-speaking churches across Benelux.";
+$projectTitle = "Belgium Women Together";
+$projectType = "Website";
+$projectYear = "2022";
+$projectTech = "HTML, CSS, JavaScript";
+$projectTeam = "";
+$projectDescription = "Belgium Women Together, as a non-denominational event, needed an independent website not linked to any one particular church. The aim was to outline the programme, introduce the speaker, give practical details and provide a way of signing up for the event.</br></br>The one-page approach was designed to be accessible and clear, paying particular attention to those viewing the site on a smartphone. The colour scheme of the site takes its cue from a piece of artwork that had been chosen by the speaker as a focus for the event.";
+$projectURL = "https://bwt-retreat.be/";
+$screenShot_1 = "public/images/screenshots/bwt/bwt-full-page.png";
+$screenShotTitle_1 = "Homepage";
+$screenShot_2 = "";
+$screenShotTitle_2 = "";
+$screenShot_3 = "";
+$screenShotTitle_3 = "";
+$screenShot_4 = "";
+$screenShotTitle_4 = "";
+$screenShot_5 = "";
+$screenShotTitle_5 = "";
+$screenShot_mobile = "public/images/screenshots/bwt/bwt-mob.png";
+$nextProjectTitle = "Boulangerie";
+$nextProjectURL = "?section=project-bakery";
+
+include("controllers/project-templateController.php");
+?>
