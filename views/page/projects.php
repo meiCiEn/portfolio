a3275ebf2daca5c53c7b528c1626db612569e940
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-08-30 15:06:43 +0200

    uploaded project image

--- views/page/projects.php
+++ views/page/projects.php
@@ -1,106 +1,145 @@
 <div class="page-bg page-bg--offwhite" style="border:5px solid red;">
+    <div class="projects d-flex">
+        <div class="projects__left d-flex" style="border: 2px solid green">
+            <div id="project__image" class="project__image">
+                <img id="project1-img" src="/public/images/test/molenbeek.png" alt="Project 1 Image" style="display: none;">
+                <img id="project2-img" src="/public/images/test/molenbeek.png" alt="Project 2 Image" style="display: none;">
+                <img id="project3-img" src="/public/images/test/molenbeek.png" alt="Project 3 Image" style="display: none;">
+                <img id="project4-img" src="/public/images/test/molenbeek.png" alt="Project 4 Image" style="display: none;">
+                <!-- Add images for other projects here -->
+            </div>
+            <div class="project__description">
 
-    <div class="d-flex">
-        <div class="projects__wrapper d-flex slide-page-up">
-            <div class="projects d-flex">
-                <div class="projects__left d-flex" style="border: 2px solid green">
+            </div>
+        </div>
+        <div class="projects__right d-flex" style="border: 2px solid green">
+            <div class="d-flex flex-column" style="border: 1px solid chartreuse;">
+                <div class="wrapper wrapper--projects" style="border:2px solid orange;">
+                    <svg id="projects" xmlns="http://www.w3.org/2000/svg" width="370.3900146484375" height="72" viewBox="0 0 370.39 72">
+                        <defs>
+                            <style>
+                                .cls-1 {
+                                    fill: #265e5d;
+                                    stroke: #265e5d;
+                                    stroke-miterlimit: 10;
+                                    stroke-width: 2px;
+                                }
+                            </style>
+                        </defs>
+                        <g id="Mears_green">
+                            <g>
+                                <path class="cls-1 svg-elem-1" d="M1,2.11H22.08c15.63,0,25.75,8.51,25.75,23.9,0,14.19-10.13,23.26-25.75,23.26H13.71c-2.4,0-4.11,1.76-4.11,4.16v16.46H1V2.11ZM13.44,41.96h7.35c11.28,0,18.45-6.15,18.45-16.14s-7.17-16.41-18.45-16.41h-7.35c-2.36,0-3.84,1.34-3.84,3.38v25.8c0,2.03,1.48,3.38,3.84,3.38Z"></path>
+                                <path class="cls-1 svg-elem-2" d="M55.76,2.11h35.09c8.51,0,14.38,5.64,14.38,13.78s-5.87,13.82-14.38,13.82h-9.34c-1.62,0-2.31,.74-2.31,1.71,0,.51,.23,1.06,.65,1.66l24.18,36.8h-9.94l-24.69-37.96c-.51-.74-1.29-2.22-2.87-2.22-1.85,0-2.17,1.48-2.17,2.59v37.59h-8.6V2.11Zm12.85,20.3h20.62c4.58,0,7.4-2.54,7.4-6.52s-2.82-6.47-7.4-6.47h-20.62c-2.64,0-4.25,1.06-4.25,3.65v5.5c0,2.77,1.66,3.84,4.25,3.84Z"></path>
+                                <path class="cls-1 svg-elem-3" d="M111.61,24.64V13.89c0-7.56,6.66-12.66,16.6-12.66s16.46,5.1,16.46,12.66v10.75c0,7.56-6.61,12.66-16.46,12.66s-16.6-5.1-16.6-12.66Zm16.6,6.97c5.32,0,8.83-3,8.83-7.4V14.39c0-4.44-3.51-7.4-8.83-7.4s-8.97,2.92-8.97,7.4v9.81c0,4.48,3.61,7.4,8.97,7.4Z"></path>
+                                <path class="cls-1 svg-elem-4" d="M113.39,47.24l8.55,.05c0,9.52,5.96,15.9,14.7,15.9s14.8-6.38,14.8-15.9V2.11h8.55V47.24c0,14.15-9.34,23.53-23.35,23.53s-23.26-9.39-23.26-23.53Z"></path>
+                                <path class="cls-1 svg-elem-5" d="M169.17,2.11h42.72v7.31h-29.87c-2.54,0-4.25,1.34-4.25,3.38v6.2c0,2.13,1.71,3.47,4.25,3.47h29.87v7.35h-29.87c-2.54,0-4.16,1.29-4.16,3.51v5.5c0,2.03,1.62,3.38,4.16,3.38l29.87-.05v7.31l-29.87,.05c-2.54,0-4.25,1.39-4.25,3.47v6.24c0,2.03,1.71,3.38,4.25,3.38h29.87v7.31h-42.72V2.11Z"></path>
+                                <path class="cls-1 svg-elem-6" d="M220.42,47.1V24.76c0-14.19,10.13-23.76,25.24-23.76s25.15,9.57,25.15,24.09v5.27h-8.55v-5.27c0-9.71-6.7-16.46-16.6-16.46s-16.6,6.75-16.6,16.78v21.13c0,10.08,6.66,16.83,16.6,16.83s16.6-6.75,16.6-16.55v-5.18h8.55v5.18c0,14.61-10.08,24.18-25.15,24.18s-25.24-9.57-25.24-23.9Z"></path>
+                                <path class="cls-1 svg-elem-7" d="M288.09,9.41h-14.56V2.11h46.7v7.31h-14.93c-2.36,0-4.16,1.71-4.16,4.16v56.31h-8.55V13.85c0-2.59-1.85-4.44-4.48-4.44Z"></path>
+                                <path class="cls-1 svg-elem-8" d="M322.09,48.44v-3.05h8.6v2.87c0,9.15,5.96,14.93,15.4,14.93,8.92,0,14.61-4.85,14.66-12.62,.05-7.12-4.76-11.51-12.71-11.65l-5.46-.14c-12.3-.28-19.74-6.94-19.74-18.31s8.97-19.28,22.42-19.28c14.29,0,23.07,7.77,23.07,20.34v2.96h-8.6v-2.77c0-7.91-5.69-12.9-14.56-12.9-8.23,0-13.73,4.53-13.73,11.42,0,6.38,4.62,10.26,12.39,10.4l5.46,.14c12.48,.28,20.11,7.44,20.11,19.28,0,12.81-9.06,20.67-23.39,20.67s-23.9-8.51-23.9-22.29Z"></path>
+                            </g>
+                        </g>
+                    </svg>
                 </div>
-                <div class="projects__right d-flex" style="border: 2px solid green">
-                    <div>
-                        <div class="wrapper wrapper--projects" style="border:2px solid orange;">
-                            <!-- <h1 class="page__title slide-up-left forestgreen">Projects</h1> -->
-                            <!--?xml version="1.0" encoding="UTF-8"?--><svg id="projects" xmlns="http://www.w3.org/2000/svg" width="370.3900146484375" height="72" viewBox="0 0 370.39 72">
-                                <defs>
-                                    <style>
-                                        .cls-1 {
-                                            fill: #265e5d;
-                                            stroke: #265e5d;
-                                            stroke-miterlimit: 10;
-                                            stroke-width: 2px;
-                                        }
-                                    </style>
-                                </defs>
-                                <g id="Mears_green">
-                                    <g>
-                                        <path class="cls-1 svg-elem-1" d="M1,2.11H22.08c15.63,0,25.75,8.51,25.75,23.9,0,14.19-10.13,23.26-25.75,23.26H13.71c-2.4,0-4.11,1.76-4.11,4.16v16.46H1V2.11ZM13.44,41.96h7.35c11.28,0,18.45-6.15,18.45-16.14s-7.17-16.41-18.45-16.41h-7.35c-2.36,0-3.84,1.34-3.84,3.38v25.8c0,2.03,1.48,3.38,3.84,3.38Z"></path>
-                                        <path class="cls-1 svg-elem-2" d="M55.76,2.11h35.09c8.51,0,14.38,5.64,14.38,13.78s-5.87,13.82-14.38,13.82h-9.34c-1.62,0-2.31,.74-2.31,1.71,0,.51,.23,1.06,.65,1.66l24.18,36.8h-9.94l-24.69-37.96c-.51-.74-1.29-2.22-2.87-2.22-1.85,0-2.17,1.48-2.17,2.59v37.59h-8.6V2.11Zm12.85,20.3h20.62c4.58,0,7.4-2.54,7.4-6.52s-2.82-6.47-7.4-6.47h-20.62c-2.64,0-4.25,1.06-4.25,3.65v5.5c0,2.77,1.66,3.84,4.25,3.84Z"></path>
-                                        <path class="cls-1 svg-elem-3" d="M111.61,24.64V13.89c0-7.56,6.66-12.66,16.6-12.66s16.46,5.1,16.46,12.66v10.75c0,7.56-6.61,12.66-16.46,12.66s-16.6-5.1-16.6-12.66Zm16.6,6.97c5.32,0,8.83-3,8.83-7.4V14.39c0-4.44-3.51-7.4-8.83-7.4s-8.97,2.92-8.97,7.4v9.81c0,4.48,3.61,7.4,8.97,7.4Z"></path>
-                                        <path class="cls-1 svg-elem-4" d="M113.39,47.24l8.55,.05c0,9.52,5.96,15.9,14.7,15.9s14.8-6.38,14.8-15.9V2.11h8.55V47.24c0,14.15-9.34,23.53-23.35,23.53s-23.26-9.39-23.26-23.53Z"></path>
-                                        <path class="cls-1 svg-elem-5" d="M169.17,2.11h42.72v7.31h-29.87c-2.54,0-4.25,1.34-4.25,3.38v6.2c0,2.13,1.71,3.47,4.25,3.47h29.87v7.35h-29.87c-2.54,0-4.16,1.29-4.16,3.51v5.5c0,2.03,1.62,3.38,4.16,3.38l29.87-.05v7.31l-29.87,.05c-2.54,0-4.25,1.39-4.25,3.47v6.24c0,2.03,1.71,3.38,4.25,3.38h29.87v7.31h-42.72V2.11Z"></path>
-                                        <path class="cls-1 svg-elem-6" d="M220.42,47.1V24.76c0-14.19,10.13-23.76,25.24-23.76s25.15,9.57,25.15,24.09v5.27h-8.55v-5.27c0-9.71-6.7-16.46-16.6-16.46s-16.6,6.75-16.6,16.78v21.13c0,10.08,6.66,16.83,16.6,16.83s16.6-6.75,16.6-16.55v-5.18h8.55v5.18c0,14.61-10.08,24.18-25.15,24.18s-25.24-9.57-25.24-23.9Z"></path>
-                                        <path class="cls-1 svg-elem-7" d="M288.09,9.41h-14.56V2.11h46.7v7.31h-14.93c-2.36,0-4.16,1.71-4.16,4.16v56.31h-8.55V13.85c0-2.59-1.85-4.44-4.48-4.44Z"></path>
-                                        <path class="cls-1 svg-elem-8" d="M322.09,48.44v-3.05h8.6v2.87c0,9.15,5.96,14.93,15.4,14.93,8.92,0,14.61-4.85,14.66-12.62,.05-7.12-4.76-11.51-12.71-11.65l-5.46-.14c-12.3-.28-19.74-6.94-19.74-18.31s8.97-19.28,22.42-19.28c14.29,0,23.07,7.77,23.07,20.34v2.96h-8.6v-2.77c0-7.91-5.69-12.9-14.56-12.9-8.23,0-13.73,4.53-13.73,11.42,0,6.38,4.62,10.26,12.39,10.4l5.46,.14c12.48,.28,20.11,7.44,20.11,19.28,0,12.81-9.06,20.67-23.39,20.67s-23.9-8.51-23.9-22.29Z"></path>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="line line--green"></div>
-                        <!-- LIST OF PROJECTS -->
-                        <ul>
-                            <li>
-                                <a class="projects__link" href="?section=project-molenbeek">
-                                    <div class="projects__row">
-                                        <div class="projects__name cursive">
-                                            <div class="projects__arrow">
-                                                <h4 class="arrow">&#x21d2;</h4>
-                                            </div>
-                                            <h4 class="projects__title uppercase">Mole&#x0272;beek en Prome&#x0272;ade</h4>
-                                        </div>
-                                        <div class="projects__year">
-                                            <p>2022</p>
-                                        </div>
+                <div class="line line--green"></div>
+                <!-- LIST OF PROJECTS -->
+                <ul>
+                    <li>
+                        <a id="project1-link" class="projects__link" href="?section=project-molenbeek">
+                            <div class="projects__row">
+                                <div class="projects__name cursive">
+                                    <div class="projects__arrow">
+                                        <h4 class="arrow">&#x21d2;</h4>
                                     </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="projects__link" href="?section=project-ecologie">
-                                    <div class="projects__row">
-                                        <div class="projects__name cursive">
-                                            <div class="projects__arrow">
-                                                <h4 class="arrow">&#x21d2;</h4>
-                                            </div>
-                                            <h4 class="projects__title uppercase">Maiso&#x0272; de l'Écologie</h4>
-                                        </div>
-                                        <div class="projects__year">
-                                            <p class="projects-year">2023</p>
-                                        </div>
+                                    <h4 class="projects__title">Molenbeek en Promenade</h4>
+                                </div>
+                                <div class="projects__year">
+                                    <p>2022</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a id="project2-link" class="projects__link" href="?section=project-molenbeek">
+                            <div class="projects__row">
+                                <div class="projects__name cursive">
+                                    <div class="projects__arrow">
+                                        <h4 class="arrow">&#x21d2;</h4>
                                     </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="projects__link" href="?section=project-bwt">
-                                    <div class="projects__row">
-                                        <div class="projects__name cursive">
-                                            <div class="projects__arrow">
-                                                <h4 class="arrow">&#x21d2;</h4>
-                                            </div>
-                                            <h4 class="projects__title uppercase">Belgium Wome&#x0272; Together</h4>
-                                        </div>
-                                        <div class="projects__year">
-                                            <p class="projects-year">2022</p>
-                                        </div>
+                                    <h4 class="projects__title">Maison de l'Écologie</h4>
+                                </div>
+                                <div class="projects__year">
+                                    <p class="projects-year">2023</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a id="project3-link" class="projects__link" href="?section=project-molenbeek">
+                            <div class="projects__row">
+                                <div class="projects__name cursive">
+                                    <div class="projects__arrow">
+                                        <h4 class="arrow">&#x21d2;</h4>
                                     </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="projects__link" href="?section=project-boulangerie">
-                                    <div class="projects__row">
-                                        <div class="projects__name cursive">
-                                            <div class="projects__arrow">
-                                                <h4 class="arrow">&#x21d2;</h4>
-                                            </div>
-                                            <h4 class="projects__title uppercase">Boulangerie</h4>
-                                        </div>
-                                        <div class="projects__year">
-                                            <p class="projects-year">2022</p>
-                                        </div>
+                                    <h4 class="projects__title">Belgium Women Together</h4>
+                                </div>
+                                <div class="projects__year">
+                                    <p class="projects-year">2022</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a id="project4-link" class="projects__link" href="?section=project-molenbeek">
+                            <div class="projects__row">
+                                <div class="projects__name cursive">
+                                    <div class="projects__arrow">
+                                        <h4 class="arrow">&#x21d2;</h4>
                                     </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                                    <h4 class="projects__title">Boulangerie</h4>
+                                </div>
+                                <div class="projects__year">
+                                    <p class="projects-year">2022</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </div>
+<script>
+    // Get references to project links and project images by ID
+    const projectLinks = {
+        'project1-link': 'project1-img',
+        'project2-link': 'project2-img',
+        // Add more link-image pairs here
+    };
+
+    // Add hover event listeners to project links
+    for (const linkId in projectLinks) {
+        const link = document.getElementById(linkId);
+        const imageId = projectLinks[linkId];
+        const image = document.getElementById(imageId);
+
+        if (link && image) {
+            link.addEventListener('mouseenter', () => {
+                // Hide all images
+                for (const imgId in projectLinks) {
+                    const img = document.getElementById(projectLinks[imgId]);
+                    if (img) {
+                        img.style.display = 'none';
+                    }
+                }
+
+                // Show the image corresponding to the hovered project
+                image.style.display = 'block';
+            });
+
+            link.addEventListener('mouseleave', () => {
+                // Hide the image when the mouse leaves the project link
+                image.style.display = 'none';
+            });
+        }
+    }
+</script>
