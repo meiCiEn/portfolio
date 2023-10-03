503c4e7fa3e5c4fafd31361c001e4e48a6f961c8
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-10-03 15:45:23 +0200

    changes to style sheets

--- views/page/projects.php
+++ views/page/projects.php
@@ -18,19 +18,9 @@ $particleColor = "#ffffff"
     <div id="tsparticles"></div>
 
     <div class="projects d-flex article">
-        <div class="projects__left">
-            <div id="project__image" class="project__image">
-                <img id="project-molenbeek-img" src="public/images/thumbnails/molenbeek.png" alt="screenshot of Molenbeek en Promenade" style="display: none;">
-                <img id="project-ecologie-img" src="public/images/thumbnails/ecologie.png" alt="Project 2 Image" style="display: none;">
-                <img id="project-bwt-img" src="public/images/thumbnails/bwt.png" alt="Project 3 Image" style="display: none;">
-                <img id="project-boulangerie-img" src="public/images/thumbnails/boulangerie.png" alt="Project 4 Image" style="display: none;">
-                <!-- Add images for other projects here -->
-            </div>
-            <div class="project__description">
-                <!-- <p>Here's a little info about the project.</p> -->
-            </div>
+        <div class="projects-left">
         </div>
-        <div class="projects__right d-flex">
+        <div class="projects-right d-flex">
             <div class="d-flex flex-column">
                 <div id="projects-svg" class="wrapper wrapper--projects">
                     <!-- svg added dynamically (see language_toggle.js) -->
@@ -48,16 +38,16 @@ $particleColor = "#ffffff"
 
                         foreach ($projects as $project) : ?>
                             <li>
-                                <a id="<?php echo $project['id']; ?>" class="projects__link" href="?section=<?php echo $project['id']; ?>">
-                                    <div class="projects__row">
-                                        <div class="projects__name cursive">
-                                            <div class="projects__arrow">
+                                <a id="<?php echo $project['id']; ?>" class="projects-link" href="?section=<?php echo $project['id']; ?>">
+                                    <div class="projects-row">
+                                        <div class="projects-name">
+                                            <div class="projects-arrow">
                                                 <h4 class="arrow">&#x21d2;</h4>
                                             </div>
-                                            <h4 class="projects__title"><?php echo $project['title']; ?></h4>
+                                            <h4 class="projects-title"><?php echo $project['title']; ?></h4>
                                         </div>
-                                        <div class="projects__year">
-                                            <p class="projects-year"><?php echo $project['year']; ?></p>
+                                        <div class="projects-year">
+                                            <p><?php echo $project['year']; ?></p>
                                         </div>
                                     </div>
                                 </a>
@@ -78,42 +68,3 @@ $particleColor = "#ffffff"
     let jsParticleColor = "<?php echo $particleColor; ?>";
 </script>
 
-
-
-<!-- <script>
-    // Get references to project links and project images by ID
-    const projectLinks = {
-        'project-molenbeek-link': 'project-molenbeek-img',
-        'project-ecologie-link': 'project-ecologie-img',
-        'project-bwt-link': 'project-bwt-img',
-        'project-boulangerie-link': 'project-boulangerie-img',
-        // Add more link-image pairs here
-    };
-
-    // Add hover event listeners to project links
-    for (const linkId in projectLinks) {
-        const link = document.getElementById(linkId);
-        const imageId = projectLinks[linkId];
-        const image = document.getElementById(imageId);
-
-        if (link && image) {
-            link.addEventListener('mouseenter', () => {
-                // Hide all images
-                for (const imgId in projectLinks) {
-                    const img = document.getElementById(projectLinks[imgId]);
-                    if (img) {
-                        img.style.display = 'none';
-                    }
-                }
-
-                // Show the image corresponding to the hovered project
-                image.style.display = 'block';
-            });
-
-            link.addEventListener('mouseleave', () => {
-                // Hide the image when the mouse leaves the project link
-                image.style.display = 'none';
-            });
-        }
-    }
-</script> -->
